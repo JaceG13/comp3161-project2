c1e2490881c031fcbdbf393fe9b2173fbf40be54
jaceg13 <jaygooden@hotmail.com>
2017-04-05 17:26:50 +0000

    changes to schema

--- database.sql
+++ database.sql
@@ -2,30 +2,39 @@ drop database if exists comp3161_proj;
 create database if not exists comp3161_proj;
 use comp3161_proj;
 
-CREATE TABLE Patient (pID varchar(6) PRIMARY KEY,
+CREATE TABLE Patient (pID varchar(8) PRIMARY KEY,
 	pNum INT(7),
 	pFName VARCHAR(30) NOT NULL,
-	pLname VARCHAR(30) NOT NULL);
+	pLname VARCHAR(30) NOT NULL,
+	pDob date not null);
 
-CREATE TABLE Family_History(famID varchar(6) PRIMARY KEY,
+CREATE TABLE Family_History(famID varchar(8) PRIMARY KEY,
 	famFName VARCHAR(30) NOT NULL,
 	famLName VARCHAR(30) NOT NULL);
 
-CREATE TABLE Doctor(docID varchar(6) PRIMARY KEY,
+CREATE TABLE Doctor(docID varchar(8) PRIMARY KEY,
 	docFName VARCHAR(30) NOT NULL,
 	docLName VARCHAR(30) NOT NULL,
-	dNum INT(7) NOT NULL);
+	dNum INT(7) NOT NULL,
+	dDob date not null);
 
-CREATE TABLE Consultant(docID varchar(6) NOT NULL primary key,
+create table Intern(docID varchar(8) primary key,
+	foreign key (docID) references Doctor(docID) on update cascade on delete cascade);
+	
+create table Resident(docID varchar(8) primary key,
+	foreign key (docID) references Doctor(docID) on update cascade on delete cascade);
+
+CREATE TABLE Consultant(docID varchar(8) NOT NULL primary key,
 	specialization VARCHAR(50) NOT NULL,
 	FOREIGN KEY (docID) references Doctor(docID) on update cascade on delete cascade);
 
-CREATE TABLE Nurse(nID varchar(6) PRIMARY KEY,
+CREATE TABLE Nurse(nID varchar(8) PRIMARY KEY,
 	nFName VARCHAR(30) NOT NULL,
 	nLName VARCHAR(30) NOT NULL,
-	dob DATE NOT NULL,
+	nDob DATE NOT NULL,
 	address VARCHAR(50) NOT NULL,
-	nurNum INT(7) NOT NULL);
+	nurNum INT(7) NOT NULL
+	);
 
 CREATE TABLE Disease(disease_ID varchar(6) PRIMARY KEY,
 	disease_name VARCHAR(50) NOT NULL
@@ -35,23 +44,23 @@ CREATE TABLE Medication(med_ID varchar(6) PRIMARY KEY,
 	med_name VARCHAR(50) NOT NULL
 	);
 
-CREATE TABLE Allergies(pID varchar(6),
+CREATE TABLE Allergies(pID varchar(8),
 	med_ID varchar(6) NOT NULL,
 	primary key (pID,med_ID),
 	FOREIGN KEY (pID) references Patient(pID) on delete cascade on update cascade,
 	FOREIGN KEY (med_ID) references Medication(med_ID) on delete cascade on update cascade
 	);
 	
-CREATE TABLE Accesses(nID varchar(6) NOT NULL,
-	pID varchar(6) NOT NULL,
-	famID varchar(6) NOT NULL,
+CREATE TABLE Accesses(nID varchar(8) NOT NULL,
+	pID varchar(8) NOT NULL,
+	famID varchar(8) NOT NULL,
 	PRIMARY KEY (nID,pID,famID),
 	FOREIGN KEY (nID) references Nurse(nID) on delete cascade on update cascade,
 	FOREIGN KEY (pID) references Patient(pID) on delete cascade on update cascade,
 	FOREIGN KEY (famID) references Family_History(famID) on delete cascade on update cascade);
 
-CREATE TABLE Administers_Medication_To(nID varchar(6),
-	pID varchar(6) NOT NULL,
+CREATE TABLE Administers_Medication_To(nID varchar(8),
+	pID varchar(8) NOT NULL,
 	med_ID varchar(6) NOT NULL,
 	process VARCHAR(50) NOT NULL,
 	med_date date,
@@ -60,39 +69,40 @@ CREATE TABLE Administers_Medication_To(nID varchar(6),
 	FOREIGN KEY (pID) references Patient(pID) on delete cascade on update cascade,
 	FOREIGN KEY (med_ID) references Medication(med_ID) on delete cascade on update cascade);
 
-CREATE TABLE Updates(nID varchar(6),
-	pID varchar(6) NOT NULL,
+CREATE TABLE Updates(nID varchar(8),
+	pID varchar(8) NOT NULL,
 	primary key (nID,pID),
 	vital_signs VARCHAR(50) NOT NULL,
 	FOREIGN KEY (nID) references Nurse(nID) on delete cascade on update cascade);
 
-CREATE TABLE Belongs_To(pID varchar(6),
-	famID varchar(6) NOT NULL,
+CREATE TABLE Belongs_To(pID varchar(8),
+	famID varchar(8) NOT NULL,
 	primary key(pID,famID),
 	FOREIGN KEY (famID) references Family_History(famID) on delete cascade on update cascade,
 	FOREIGN KEY (pID) references Patient(pID) on delete cascade on update cascade);
 
-CREATE TABLE Present_In(famID varchar(6),
+CREATE TABLE Present_In(famID varchar(8),
 	disease_ID varchar(6) NOT NULL,
 	primary key(famID,disease_ID),
 	FOREIGN KEY (famID) references Family_History(famID) on delete cascade on update cascade,
 	FOREIGN KEY (disease_ID) references Disease(disease_ID) on delete cascade on update cascade);
 
-CREATE TABLE Records_Data_On(pID varchar(6),
-	famID varchar(6) NOT NULL,
-	docID varchar(6) NOT NULL,
+CREATE TABLE Records_Data_On(pID varchar(8),
+	famID varchar(8) NOT NULL,
+	docID varchar(8) NOT NULL,
 	primary key(pID,famID,docID),
 	FOREIGN KEY (pID) references Patient(pID) on delete cascade on update cascade,
 	FOREIGN KEY (famID) references Family_History (famID) on delete cascade on update cascade,
 	FOREIGN KEY (docID) references Doctor (docID) on delete cascade on update cascade);
 
 create table Diagnosis(diagID varchar(6) not null primary key,
-	diagName varchar(50) not null,
-	diag_date date
+	disease_ID varchar(6) not null,
+	diag_date date,
+	foreign key (disease_ID) references Disease(disease_ID) on update cascade on delete cascade
 );
 
-CREATE TABLE Records_Medicals_For(pID varchar(6),
-	docID varchar(6) NOT NULL,
+CREATE TABLE Records_Medicals_For(pID varchar(8),
+	docID varchar(8) NOT NULL,
 	diagID varchar(6),
 	date_created date,
 	date_modified date,
@@ -106,8 +116,11 @@ CREATE TABLE Records_Medicals_For(pID varchar(6),
 	FOREIGN KEY (diagID) references Diagnosis(diagID) on delete cascade on update cascade);
 
 delimiter //
-create procedure getAllergies(patient varchar(6))
+create procedure getAllergies(patient varchar(8))
 BEGIN
 	select pID, Medication.med_ID, med_name from Allergies join Medication on Allergies.med_ID = Medication.med_ID where pID = patient;
 END//
 DELIMITER ;
+
+delimiter //
+create procedure
