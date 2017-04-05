f5a79ab326064a4f9a4fd39cdbc883bcb4e03e92
jaceg13 <jaygooden@hotmail.com>
2017-04-05 20:58:42 +0000

    all queries functional

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ drop database if exists comp3161_proj;
 create database if not exists comp3161_proj;
 use comp3161_proj;
 
-CREATE TABLE Patient (pID varchar(8) PRIMARY KEY,
+CREATE TABLE Patient (pID varchar(8) not null PRIMARY KEY,
 	pNum INT(7),
 	pFName VARCHAR(30) NOT NULL,
 	pLname VARCHAR(30) NOT NULL,
@@ -58,6 +58,22 @@ CREATE TABLE Accesses(nID varchar(8) NOT NULL,
 	FOREIGN KEY (nID) references Nurse(nID) on delete cascade on update cascade,
 	FOREIGN KEY (pID) references Patient(pID) on delete cascade on update cascade,
 	FOREIGN KEY (famID) references Family_History(famID) on delete cascade on update cascade);
+	
+create table Secretary(
+	sID varchar(8) not null primary key,
+	sFName varchar(50),
+	sLName varchar(50),
+	sDob DATE,
+	sNum int(7)
+);
+
+create table Registers(
+	sID varchar(8) not null,
+	pID varchar(8) not null,
+	primary key (sID,pID),
+	FOREIGN KEY (pID) references Patient(pID) on delete cascade on update cascade,
+	FOREIGN KEY (sID) references Secretary(sID) on delete cascade on update cascade
+	);
 
 CREATE TABLE Administers_Medication_To(nID varchar(8),
 	pID varchar(8) NOT NULL,
@@ -71,8 +87,8 @@ CREATE TABLE Administers_Medication_To(nID varchar(8),
 
 CREATE TABLE Updates(nID varchar(8),
 	pID varchar(8) NOT NULL,
-	primary key (nID,pID),
 	vital_signs VARCHAR(50) NOT NULL,
+	primary key (nID,pID),
 	FOREIGN KEY (nID) references Nurse(nID) on delete cascade on update cascade);
 
 CREATE TABLE Belongs_To(pID varchar(8),
@@ -95,9 +111,12 @@ CREATE TABLE Records_Data_On(pID varchar(8),
 	FOREIGN KEY (famID) references Family_History (famID) on delete cascade on update cascade,
 	FOREIGN KEY (docID) references Doctor (docID) on delete cascade on update cascade);
 
-create table Diagnosis(diagID varchar(6) not null primary key,
+create table Diagnosis(diagID varchar(6) not null,
 	disease_ID varchar(6) not null,
+	pID varchar(8) not null,
 	diag_date date,
+	primary key (diagID,disease_ID,pID),
+	foreign key (pID) references Patient(pID) on update cascade on delete cascade,
 	foreign key (disease_ID) references Disease(disease_ID) on update cascade on delete cascade
 );
 
@@ -115,6 +134,7 @@ CREATE TABLE Records_Medicals_For(pID varchar(8),
 	FOREIGN KEY (docID) references Doctor(docID) on delete cascade on update cascade,
 	FOREIGN KEY (diagID) references Diagnosis(diagID) on delete cascade on update cascade);
 
+
 delimiter //
 create procedure getAllergies(patient varchar(8))
 BEGIN
@@ -122,5 +142,68 @@ BEGIN
 END//
 DELIMITER ;
 
+
+
 delimiter //
-create procedure
+create procedure searchByDiagnosis(diag varchar(50), date1 date, date2 date)
+begin
+	Select pFName, pLName, Patient.pID From Diagnosis join Patient on(Diagnosis.pID = Patient.pID) join Records_Medicals_For on
+	(Diagnosis.pID = Records_Medicals_For.pID) join Disease on Diagnosis.disease_ID = Disease.disease_ID Where disease_name = diag and diag_date between date1 and date2;
+end//
+delimiter ;
+
+
+
+-- delimiter //
+-- create procedure mostAllergic
+-- begin
+-- 	Select count(med_ID)
+-- 	from allergies
+-- 	group by med_ID
+
+
+
+delimiter //
+create procedure mostAllergic()
+begin
+	SELECT       Medication.med_ID, med_name, COUNT(Medication.med_ID) AS frequentMedicine
+	    FROM     Allergies join Medication on Allergies.med_ID = Medication.med_ID
+	    GROUP BY  Medication.med_ID
+	    ORDER BY frequentMedicine DESC
+	    LIMIT    1;
+end//
+delimiter ;
+
+
+
+delimiter //
+create procedure allTests(patient varchar(8))
+begin
+	Select test_results, scans from Records_Medicals_For where pID = patient;
+end//
+delimiter ;
+
+
+
+delimiter //
+create procedure allNurses(date1 date)
+begin
+	Select Nurse.nID, nFName, nLName
+	from Nurse join Administers_Medication_To on(Nurse.nID = Administers_Medication_To.nID)
+	where med_date = date1;
+end//
+delimiter ;
+
+
+
+delimiter //
+create procedure allInterns()
+begin
+	Select docFName, docLName, count(Intern.docID) as docCount from Intern join Records_Medicals_For on (Intern.docID = Records_Medicals_For.docID)
+	join Doctor on (Doctor.docID = Intern.docID)
+	group by Intern.docID
+	order by docCount DESC
+	Limit 1;
+end//
+delimiter ;
+
